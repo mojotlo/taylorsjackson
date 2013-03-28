2290cbb3ea86b4c528db33d1dc39a712f755b4e4
Taylor Jackson <tsjackson@gmail.com>
2013-03-27 23:50:39 -0500

    spec deletion?

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Taylorsjackson::Application.routes.draw do
   get "blog/edit"
 
   root :to  =>  'pages#home'
-  match  '/contact' => 'pages#contact'
+  match  '/contact' => 'pages#contacto'
   match  '/work' => 'pages#work'
   match  '/blog' => 'blog#index'
 
